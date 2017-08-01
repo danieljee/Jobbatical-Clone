c14bbb54068e5630155c2993963e65432551bdd4
SharonStrats <sstratsianis@yahoo.com>
2017-08-01 13:39:43 +1000

    Testing merge

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,8 @@
 var controllers = require('../controllers');
 var express = require('express');
 var router = express.Router();
+
+// SAM want to look at this further to see how it works
 module.exports = function(){
 	router.route('/:resource')
 		.post(function(req, res, next){
